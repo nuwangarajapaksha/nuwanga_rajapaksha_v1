8308f7312b1f099d55ac7d1b82215cf8e99aada3
@nuwanga <nuwanga@xdoto.io>
2025-07-28 01:01:33 +0530

    finish v1.

--- photography/views.py
+++ photography/views.py
@@ -5,20 +5,40 @@ from django.shortcuts import render
 
 def photography(request):
 
-    cameras_row1 = [
+    cameras = [
         'nikon-d7200'
     ]
 
-    lenses_row2 = [
-        'nikon-18-55', 'nikon-18-55'
+    lenses = [
+        'nikon-18-55', 'nikon-70-300'
     ]
 
-    tech_icons_row3 = [
+    tech_icons = [
         'adobe-ps', 'adobe-lr'
     ]
 
+    wildlife_images = [
+        'wildlife02', 'wildlife03', 'wildlife04', 'wildlife05', 'wildlife06', 'wildlife07'
+    ]
+
+    model_images = [
+        'model02', 'model03', 'model04', 'model05', 'model06', 'model07'
+    ]
+
+    event_images = [
+        'event02', 'event03', 'event04', 'event05', 'event06', 'event07'
+    ]
+
+    landscape_images = [
+        'landscape02', 'landscape03', 'landscape04', 'landscape05', 'landscape06', 'landscape07'
+    ]
+
     return render(request, 'photography.html', {
-        'cameras_row1': cameras_row1,
-        'lenses_row2': lenses_row2,
-        'tech_icons_row3': tech_icons_row3,
+        'cameras': cameras,
+        'lenses': lenses,
+        'tech_icons': tech_icons,
+        'wildlife_images': wildlife_images,
+        'model_images': model_images,
+        'event_images': event_images,
+        'landscape_images': landscape_images,
     })
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 
 
 def portfolio(request):
-    tech_icons_row1 = [
+    tech_icons = [
         'adobe', 'adobe-ps', 'adobe-ai', 'adobe-pr', 'adobe-ae', 'adobe-lr'
     ]
     return render(request, 'portfolio.html', {
-        'tech_icons_row1': tech_icons_row1,
+        'tech_icons': tech_icons,
     })
