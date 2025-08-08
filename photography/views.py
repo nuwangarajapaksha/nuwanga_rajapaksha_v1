2de9fb20597ab56db7154eb8ff2988379a167c2d
@nuwanga <nuwanga@xdoto.io>
2025-08-08 11:00:22 +0530

    Update Projects 5.

--- photography/views.py
+++ photography/views.py
@@ -4,41 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def photography(request):
-
-    cameras = [
-        'nikon-d7200'
-    ]
-
-    lenses = [
-        'nikon-18-55', 'nikon-70-300'
-    ]
-
-    tech_icons = [
-        'adobe-ps', 'adobe-lr'
-    ]
-
-    wildlife_images = [
-        'wildlife02', 'wildlife03', 'wildlife04', 'wildlife05', 'wildlife06', 'wildlife07'
-    ]
-
-    model_images = [
-        'model02', 'model03', 'model04', 'model05', 'model06', 'model07'
-    ]
-
-    event_images = [
-        'event02', 'event03', 'event04', 'event05', 'event06', 'event07'
-    ]
-
-    landscape_images = [
-        'landscape02', 'landscape03', 'landscape04', 'landscape05', 'landscape06', 'landscape07'
-    ]
-
-    return render(request, 'photography.html', {
-        'cameras': cameras,
-        'lenses': lenses,
-        'tech_icons': tech_icons,
-        'wildlife_images': wildlife_images,
-        'model_images': model_images,
-        'event_images': event_images,
-        'landscape_images': landscape_images,
-    })
+    context = {
+        'cameras': ['nikon-d7200'],
+        'lenses': ['nikon-18-55', 'nikon-70-300'],
+        'tech_icons': ['adobe-ps', 'adobe-lr'],
+        'wildlife_images': [f'wildlife{i:02d}' for i in range(2, 8)],
+        'model_images': [f'model{i:02d}' for i in range(2, 8)],
+        'event_images': [f'event{i:02d}' for i in range(2, 8)],
+        'landscape_images': [f'landscape{i:02d}' for i in range(2, 8)],
+    }
+    return render(request, 'photography.html', context)
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 
 
 def portfolio(request):
-    tech_icons = [
-        'adobe', 'adobe-ps', 'adobe-ai', 'adobe-pr', 'adobe-ae', 'adobe-lr'
-    ]
-    return render(request, 'portfolio.html', {
-        'tech_icons': tech_icons,
-    })
+    context = {
+        'tech_icons': [
+            'adobe', 'adobe-ps', 'adobe-ai', 'adobe-pr', 'adobe-ae', 'adobe-lr'
+        ]
+    }
+    return render(request, 'portfolio.html', context)
